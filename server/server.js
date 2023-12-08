b834c38fc71c2fefed7228486a73b9ffb1438996
Utkarshkumar29 <79745691+Utkarshkumar29@users.noreply.github.com>
2023-12-08 13:53:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,13 @@ const path = require('path');
 const fs=require('fs');
 const BookingModel = require('./models/booking');
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://mern-stack-website-api.vercel.app],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use(express.json());
 app.use(cookieParser()); 
 app.use('/uploads',express.static(__dirname+'/'))
